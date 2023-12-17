ea14775bcde7bca45052012670deb5ffad52582c
Vladislav <vladikonoo@gmail.com>
2023-12-17 05:53:27 +0300

    added not-found page and btn-up

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,29 @@
-document.addEventListener('scroll', (e) => {
+const btnUp = {
+        el: document.querySelector('.btn-up'),
+        show() {
 
-        document.querySelector('.back-to').style.display = (window.pageYOffset > 250) ? 'flex' : 'none';
-});
+          this.el.classList.remove('btn-up_hide');
+        },
+        hide() {
+
+          this.el.classList.add('btn-up_hide');
+        },
+        addEventListener() {
+
+          window.addEventListener('scroll', () => {
+
+            const scrollY = window.scrollY || document.documentElement.scrollTop;
+            
+            scrollY > 400 ? this.show() : this.hide();
+          });
+          document.querySelector('.btn-up').onclick = () => {
+            window.scrollTo({
+              top: 0,
+              left: 0,
+              behavior: 'smooth'
+            });
+          }
+        }
+      }
+      
+      btnUp.addEventListener();
